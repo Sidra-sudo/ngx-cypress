cfec29e2622075438e24e218655d2a5ef7893ce5
Sidra-sudo <sidra.mushtaq@devsinc.com>
2022-05-25 15:27:12 +0100

    First Commit

--- cypress/integration/ngx-layout-locators.js
+++ cypress/integration/ngx-layout-locators.js
@@ -1,83 +1,112 @@
 class LayoutFeatures{
 
     ClickLayout(){
-        return cy.get('Layout')
+        return cy.contains('Layout')
     }
+
+    //Click on Stepper
     ClickStepper(){
-        return cy.get('Stepper')
+        return cy.contains('Stepper')
     }
     //In Stepper 1
     ClickNext1(){
-        return cy.get('#next1')
+        return cy.get('[test-id="next1"]')
     }
     ClickNext2(){
-        return cy.get('#next2')
+        return cy.get('[test-id="next2"]')
     }
     ClickNext3(){
-        return cy.get('#next3')
+        return cy.get('[test-id="next3"]')
     }
     ClickPrev2(){
-        return cy.get('#prev2')
+        return cy.get('[test-id="prev2"]')
     }
     ClickPrev3(){
-        return cy.get('#prev3')
+        return cy.get('[test-id="prev3"]')
     }
     ClickPrev4(){
-        return cy.get('#prev4')
+        return cy.get('[test-id="prev4"]')
     }
 
     //In Stepper 2
 
     Enteryourname(){
-        return cy.get('#EnterName')
+        return cy.get('[test-id="EnterName"]')
     }
     EnterfavMovie(){
-        return cy.get('#EnterMovie')
+        return cy.get('[test-id="EnterMovie"]')
     }
     EnterSomething(){
-        return cy.get('#EnterSomething')
+        return cy.get('[test-id="EnterSomething"]')
     }
     ClickNameNext(){
-        return cy.get('#next5')
+        return cy.get('[test-id="next5"]')
     }
     ClickMovieNext(){
-        return cy.get('#next6')
+        return cy.get('[test-id="next6"]')
     }
     ClickConfirmSomething(){
-        return cy.get('#confirm')
+        return cy.get('[test-id="confirm"]')
     }
     ClickPrevName(){
-        return cy.get('#prev5')
+        return cy.get('[test-id="prev5"]')
     }
     ClickPrevMovie(){
-        return cy.get('#prev6')
+        return cy.get('[test-id="prev6"]')
     }
     ClickonAgain(){
-        return cy.get('#tryagain')
+        return cy.get('[test-id="tryagain"]')
     }
-    ProfitCardHeading(){
-        return cy.contains('span', 'Profit')
-    }
-
     //In Stepper 3
     ClickFirstNext(){
-        return cy.get('#next7')
+        return cy.get('[test-id="next7"]')
     }
     ClickSecondNext(){
-        return cy.get('#next8')
+        return cy.get('[test-id="next8"]')
     }
     ClickThirdNext(){
-        return cy.get('#next9')
+        return cy.get('[test-id="next9"]')
     }
     ClickSecondPrev(){
-        return cy.get('#prev8')
+        return cy.get('[test-id="prev8"]')
     }
     ClickThirdPrev(){
-        return cy.get('#prev9')
+        return cy.get('[test-id="prev9"]')
     }
     ClickForthPrev(){
-        return cy.get('#prev10')
+        return cy.get('[test-id="prev10"]')
     }
+
+    //Click on Accordion
+    ClickAccordion(){
+        return cy.contains('Accordion')
+    }
+    Clicksidebartoggle(){
+        return cy.get('[test-id="sidebar"]')
+    }
+    ClickToggleFirstbutton(){
+        return cy.contains('button','Toggle First Item')
+        // .contains('button','Toggle First Item')
+    }
+    ClickProductDetail(){
+        return cy.get('[test-id="Product"]')
+    }
+    ClickReview(){
+        return cy.get('[test-id="Review"]')
+    }
+    ClickEdit(){
+        return cy.get('[test-id="Edit"]')
+    }
+    ClickProductDropdown(){
+        return cy.get('[test-id="Product2"]')
+    }
+    ClickReviewDropdown(){
+        return cy.get('[test-id="reviews2"]')
+    }
+    ClickEditDropdown(){
+        return cy.get('[test-id="edit2"]')
+    }
+
 
 
 }
--- cypress/integration/ngx-layout.spec.js
+++ cypress/integration/ngx-layout.spec.js
@@ -4,30 +4,162 @@ describe('NGX Admin Layout Feature', () => {
     const Layout = new LayoutFeatures();
 
     before(() => {
-        cy.visit('/')
+        cy.visit('http://localhost:4200/')
     })
-    context('Layout', () => {
+    context('Layout Stepper1', () => {
         it('Should verify the layout feature is clickable', () => {
-            Layout.ProfitCardPosition()
-            .should('have.css', 'display')
-            .and('match', /inline/)
-        })
-        it('Should verify the heading of the profit card', () => {
-            EComCards.ProfitCardHeading()
-            .should('have.text', 'Profit')
-        })
-        it('Should allow user to flip the profit card', () => {
-            EComCards.ProfitCardFlip1()
+            Layout.ClickLayout()
             .click()
         })
-        it('Should allow user to flip back the profit card', () => {
-            EComCards.ProfitCardFlip2()
+        it('Should verify Stepper is under Layout is clickable', () => {
+            Layout.ClickStepper()
             .click()
         })
-        it('Should allow user to scroll through the graph', () => {
-            EComCards.ProfitStatsChart()
-            .trigger('mousemove')
+        it('Should allow user to click on Next button from First Step', () => {
+            Layout.ClickNext1()
+            .click()
+        })
+        it('Should allow user to click on Next button from Second Step', () => {
+            Layout.ClickNext2()
+            .click()
+        })
+        it('Should allow user to click on Next button from Third Step', () => {
+            Layout.ClickNext3()
+            .click()
+        })
+        it('Should allow user to click on Prev button from Forth Step', () => {
+            Layout.ClickPrev4()
+            .click()
+        })
+        it('Should allow user to click on Prev button from Third Step', () => {
+            Layout.ClickPrev3()
+            .click()
+        })
+        it('Should allow user to click on Prev button from Second Step', () => {
+            Layout.ClickPrev2()
+            .click()
         })
 
+    context('Layout Stepper2', () => {
+            it('Should verify that user is able to Enter Name in Name field', () => {
+                Layout.Enteryourname()
+                .type('Sidra')
+            })
+            it('Should verify that user is able to click on Next button after entering Name', () => {
+                Layout.ClickNameNext()
+                .click()
+            })
+            it('Should verify that user is able to Enter Movie Name in Movie field', () => {
+                Layout.EnterfavMovie()
+                .type('Movie:XYZ')
+            })
+            it('Should verify that user is able to click on Next button after entering Movie Name', () => {
+                Layout.ClickMovieNext()
+                .click()
+            })
+            it('Should verify that user is able to Enter Something in text field', () => {
+                Layout.EnterSomething()
+                .type('Hello, World')
+            })
+            it('Should verify that user is able to click on sidebar toggle to close list of features', () => {
+                Layout.Clicksidebartoggle()
+                .click()
+            })
+            it('Should verify that user is able to click on Prev button after entering Something', () => {
+                Layout.ClickPrevMovie()
+                .click()
+            })
+            it('Should verify that user is able to click on Prev button From Movie Step', () => {
+                Layout.ClickPrevName()
+                .click()
+            })
+            it('Should verify that user is able to click on Next button From Name Step', () => {
+                Layout.ClickNameNext()
+                .click()
+            })
+            it('Should verify that user is able to click on Next button From Movie Step', () => {
+                Layout.ClickMovieNext()
+                .click()
+            })
+            it('Should verify that user is able to click on Confirm button From Last Step', () => {
+                Layout.ClickConfirmSomething()
+                .click()
+            })
+            it('Should verify that user is able to click on Try Again button when Wizard completed!', () => {
+                Layout.ClickonAgain()
+                .click()
+            })
     })
+    context('Layout Stepper3', () => {
+        it('Should allow user to click on Next button from First Step', () => {
+            Layout.ClickFirstNext()
+            .click()
+        })
+        it('Should allow user to click on Next button from Second Step', () => {
+            Layout.ClickSecondNext()
+            .click()
+        })
+        it('Should allow user to click on Next button from Third Step', () => {
+            Layout.ClickThirdNext()
+            .click()
+        })
+        it('Should allow user to click on Prev button from Forth Step', () => {
+            Layout. ClickForthPrev()
+            .click()
+        })
+        it('Should allow user to click on Prev button from Third Step', () => {
+            Layout.ClickThirdPrev()
+            .click()
+        })
+        it('Should allow user to click on Prev button from Second Step', () => {
+            Layout.ClickSecondPrev()
+            .click()
+        })
+    })
+
+    //Go to Accordion
+    context('Click on Accordion', () => {
+        it('Should verify that user is able to click on sidebar toggle to close list of features', () => {
+            Layout.Clicksidebartoggle()
+            .click()
+        })
+        it('Should verify that user is able to click on Accordion from Layout dropdown', () => {
+            Layout.ClickAccordion()
+            .click()
+        })
+        it('Should verify that user is able to click on sidebar toggle to close list of features', () => {
+            Layout.Clicksidebartoggle()
+            .click()
+        })
+        it('Should verify that user is able to click on Toggle First button from Accordion', () => {
+            Layout.ClickToggleFirstbutton()
+            .should('contain','Toggle First Item')
+            .click()
+        })
+        it('Should verify that user is able to click on Product Detail button from Left Toggle', () => {
+            Layout.ClickProductDetail()
+            .click()
+        })
+        it('Should verify that user is able to click on Review button from Left Toggle', () => {
+            Layout.ClickReview()
+            .click()
+        })
+        it('Should verify that user is able to click on Edit button from Left Toggle', () => {
+            Layout.ClickEdit()
+            .click()
+        })
+        it('Should verify that user is able to click on Product Detail button from Right Toggle', () => {
+            Layout.ClickProductDropdown()
+            .click()
+        })
+        it('Should verify that user is able to click on Review button from Right Toggle', () => {
+            Layout.ClickReviewDropdown()
+            .click()
+        })
+        it('Should verify that user is able to click on Edit button from Right Toggle', () => {
+            Layout.ClickEditDropdown()
+            .click()
+        })
+    })
+})
 })
